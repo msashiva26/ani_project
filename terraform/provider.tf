09011daba98ba7016a5e374cc8c7b2b490e2081c
msashiva26 <120711712+msashiva26@users.noreply.github.com>
2023-03-20 13:30:25 +0530

    updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAY4K4BSBJ56AAXPEE"
-secret_key = "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"
+access_key = "AKIAY4K4BSBJTMMWOKWG"
+secret_key = "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"
 }
