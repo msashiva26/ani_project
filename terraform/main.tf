dbd3e924218ca918586b3e7f60604517092f3096
root <root@ip-172-31-53-192.ec2.internal>
2023-03-20 06:55:41 +0000

    code ani committed

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "key" {
+ami = "ami-005f9685cb30f234b"
+instance_type = "t2.micro"
+key_name = "vasi"
+vpc_security_group_ids = [aws_security_group.sample-sg.id]
+tags = {
+Name = "prod-server"
+Environment = "dev"
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIAY4K4BSBJ5KR2XJON"
+secret_key = "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"
+}
