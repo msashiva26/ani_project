e30ef0ffbe146a5b1bddb4ee75206c1fa75078ec
msashiva26 <120711712+msashiva26@users.noreply.github.com>
2023-03-20 13:21:56 +0530

    credentials updted

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAY4K4BSBJ5KR2XJON"
-secret_key = "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"
+access_key = "AKIAY4K4BSBJ56AAXPEE"
+secret_key = "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"
 }
